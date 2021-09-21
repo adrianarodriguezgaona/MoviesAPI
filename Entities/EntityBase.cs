efa3b764b0aa809e8434a1258bf09ee4d5b8334c
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2021-09-21 15:50:37 +0200

    Using Identity Authentication

--- Entities/EntityBase.cs
+++ Entities/EntityBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoviesApi.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace MoviesApi.Entities
         public int Id { get; set; }
         [Required(ErrorMessage = "The field with name {0} is required")]
         [StringLength(60)]
+        [FirstLetterUppercase]
         public string Name { get; set; }
     }
 }
